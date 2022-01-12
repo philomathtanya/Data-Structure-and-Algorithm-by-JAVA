b833544f52df5b66df8039a5144c99e89b215a49
techieritesh <73749372+techieritesh@users.noreply.github.com>
2022-01-12 21:41:07 +0530

    remove_consecutive_element in array

---
+++ remove_consecutive_element.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class remove_consecutive_element {
+
+    public static int removeDuplicateElements(int arr[], int n){
+        if (n==0 || n==1){
+            return n;
+        }
+        int[] temp = new int[n];
+        int j = 0;
+        for (int i=0; i<n-1; i++){
+            if (arr[i] != arr[i+1]){
+                temp[j++] = arr[i];
+            }
+        }
+        temp[j++] = arr[n-1];
+        // Changing original array
+        for (int i=0; i<j; i++){
+            arr[i] = temp[i];
+        }
+        return j;
+    }
+
+    public static void main (String[] args) {
+        int n;
+        Scanner s=new Scanner(System.in);
+        n=s.nextInt();
+        int arr[] = new int[n];
+        for (int j = 0; j < n; j++) {
+            arr[j]= s.nextInt();
+        }
+        int length = arr.length;
+        length = removeDuplicateElements(arr, length);
+        //printing array elements
+        for (int i=0; i<length; i++)
+            System.out.print(arr[i]+" ");
+    }
+}
