40aa6f1b0a323f74263d9e6d8fa906eb418f2a44
Ritesh Verma <73749372+techieritesh@users.noreply.github.com>
2022-02-13 16:34:34 +0530

    insertion linkedlist node in starting

---
+++ insertion_linkedlist_starting.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+
+import java.util.Scanner;
+
+
+public class insertion_linkedlist_starting {
+    static class Node
+    {
+        int roll;
+        Node next;
+    }
+
+    static Node head;
+    insertion_linkedlist_starting()
+    {
+        head=null;
+    }
+
+    public static void main(String[] args) {
+        int t=1;
+        while(t==1) {
+
+
+            System.out.println("Enter the Roll number: ");
+            Scanner s = new Scanner(System.in);
+            Node nn = new Node();
+            int data = s.nextInt();
+            nn.roll=data;
+            nn.next=null;
+            if (head == null) {
+                head = nn;
+            } else {
+                nn.next=head;
+                head=nn;
+
+            }
+            System.out.println("Do you want to add more: (0/1) ");
+            t=s.nextInt();
+        }
+        System.out.println(" This is linkedlist:  \n");
+
+        Node c=head;
+        while(c!=null)
+        {
+            System.out.println(c.roll);
+            c=c.next;
+        }
+
+
+
+
+
+
+    }
+}
+
