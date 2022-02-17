c87be2a4f2d321ea9d8dc25a570d9910ddf7e7cd
Ritesh Verma <73749372+techieritesh@users.noreply.github.com>
2022-02-17 20:50:09 +0530

    Insertion_Doubylinkedlist_end

---
+++ Insertion_Doubylinkedlist_end.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Insertion_Doubylinkedlist_end {
+
+
+        static class node
+        {
+            int data;
+            node pre;
+            node next;
+
+        }
+        static node head=null;
+
+        public static void Display()
+        {
+            System.out.println("Linkedlist =");
+            node temp=head;
+            while(temp!=null)
+            {
+                System.out.println(temp.data);
+                temp=temp.next;
+            }
+        }
+        public static void main(String[] args) {
+            Scanner s=new Scanner(System.in);
+            int t=1;
+            while(t==1) {
+                node nn = new node();
+                System.out.println("Enter the data for nn: ");
+                int val = s.nextInt();
+                nn.data = val;
+                nn.next = null;
+
+                if (head == null) {
+                    nn.pre = null;
+                    head = nn;
+                } else {
+                   node p=head;
+                   while(p.next!=null)
+                   {
+                       p=p.next;
+                   }
+                    p.next=nn;
+                   nn.pre=p;
+
+
+                }
+                System.out.println("Press 1 to Continue & 0 to Exit! ");
+                t=s.nextInt();
+
+            }
+            Display();
+
+        }
+    }
+
