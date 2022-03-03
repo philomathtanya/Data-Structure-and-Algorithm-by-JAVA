37ee9bdbf779eed53e825fb2e4a51d9d9089b09f
Ritesh Verma <73749372+techieritesh@users.noreply.github.com>
2022-03-03 18:09:04 +0530

    reverse_linkedlist_recursion

---
+++ reverse_linkedlist_recursion.java
@@ -0,0 +1,42 @@
+public class reverse_linkedlist_recursion
+{
+    public static class node{
+        int data;
+        node next;}
+    public static void  rev(node head)
+    {
+        if(head.next!=null) {
+             rev(head.next);
+            System.out.println(head.data);
+        } else
+            System.out.println(head.data);
+
+    }
+
+    public static node head=null;
+    public static node tail=null;
+    public static void insert(int n)
+    {
+        node nn=new node();
+        nn.data=n;
+        nn.next=null;
+        if(head==null)
+        {
+            head=nn;
+            tail=nn;
+        }
+        else
+        {
+            tail.next=nn;
+            tail=nn;
+        }
+    }
+
+    public static void main(String[] args) {
+        insert(10);
+        insert(20);
+        insert(30);
+        insert(40);
+        rev(head);
+    }
+}
