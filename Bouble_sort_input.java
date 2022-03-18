c2d31aae81757865cf732d66a901655cc5fc2fca
Ritesh Verma <73749372+techieritesh@users.noreply.github.com>
2022-03-18 15:36:30 +0530

    Bouble_sort_input

---
+++ Bouble_sort_input.java
@@ -0,0 +1,35 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class Bouble_sort_input {
+    public void sort(int arr[])
+    {
+        int n=arr.length;
+        int temp;
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j+1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        for (int j = 0; j < arr.length; j++) {
+            System.out.println(arr[j]);
+        }
+
+    }
+    public static void main(String[] args) {
+        //Sorted array
+        Bouble_sort obj=new Bouble_sort();
+        int arr[]=new int[5];
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter the array: ");
+        for (int i = 0; i < 5; i++) {
+            arr[i]=s.nextInt();
+        }
+        obj.sort(arr);
+    }
+
+
+}
