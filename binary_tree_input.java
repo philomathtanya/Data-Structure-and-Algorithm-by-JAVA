dfebe1bbc4c6366d84b3be18c7d156b56b93c2ca
Ritesh Verma <73749372+techieritesh@users.noreply.github.com>
2022-04-04 22:51:06 +0530

    binary_tree_input

---
+++ binary_tree_input.java
@@ -0,0 +1,42 @@
+
+
+import java.util.Scanner;
+
+ public class binary_tree_input {
+     static class Node {
+         Node left, right;
+         int data;
+
+         public Node(int data) {
+             this.data = data;
+         }
+     }
+
+     static Scanner sc = null;
+
+     public static void main(String[] args) {
+         sc = new Scanner(System.in);
+
+         Node root = createTree();
+
+     }
+
+     static Node createTree() {
+
+         Node root = null;
+         System.out.println("Enter data: ");
+         int data = sc.nextInt();
+
+         if (data == -1) return null;
+
+         root = new Node(data);
+
+         System.out.println("Enter left for " + data);
+         root.left = createTree();
+
+         System.out.println("Enter right for " + data);
+         root.right = createTree();
+
+         return root;
+     }
+ }
